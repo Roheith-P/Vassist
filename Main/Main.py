160ab10513b54e00a0f28d0ec1b59f087a68e85f
Roheith-P <86424952+Roheith-P@users.noreply.github.com>
2022-03-10 11:39:29 +0530

    Update 1.2

--- Main/Main.py
+++ Main/Main.py
@@ -1,9 +1,22 @@
+import ipaddress
+import time
+import webbrowser
+
 print("Enter Your Commands")
 i = 1
 while i < 6:
-  iput = input(">>>")
-  if iput == "Hello":
-    print("hi")
-
+    iput = input(">>>")
+    if iput == "Hello":
+        print("hi")
+    elif iput == "about":
+        f = open("ver.txt", "r")
+        print(f.read())
+    elif iput == "time":
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(current_time)
+    elif iput == "openlink":
+        url = input("Input Your Url:")
+        webbrowser.open(url)
 
 i += 1
