1dc8ddf010eb3d898fe7d2d813c13654d5f7a7c0
Roheith-P <86424952+Roheith-P@users.noreply.github.com>
2022-03-10 11:54:40 +0530

    Update Version : 1.2

--- Main/Main.py
+++ Main/Main.py
@@ -1,6 +1,8 @@
 import ipaddress
 import time
 import webbrowser
+import requests
+from bs4 import BeautifulSoup
 
 print("Enter Your Commands")
 i = 1
@@ -18,5 +20,39 @@ while i < 6:
     elif iput == "openlink":
         url = input("Input Your Url:")
         webbrowser.open(url)
+    elif iput == "weather":
 
+
+        # enter city name
+        city = "lucknow"
+
+        # creating url and requests instance
+        url = "https://www.google.com/search?q=" + "weather" + city
+        html = requests.get(url).content
+
+        # getting raw data
+        soup = BeautifulSoup(html, 'html.parser')
+        temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+        str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+        # formatting data
+        data = str.split('\n')
+        time = data[0]
+        sky = data[1]
+
+        # getting all div tag
+        listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+        strd = listdiv[5].text
+
+        # getting other required data
+        pos = strd.find('Wind')
+        other_data = strd[pos:]
+
+        # printing all data
+        print("Temperature is", temp)
+        print("Time: ", time)
+        print("Sky Description: ", sky)
+        print(other_data)
+    elif iput == "help":
+        print("Ask Anything If You Want To!")
 i += 1
